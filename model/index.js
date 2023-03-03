30ab8ba550ea7cdb732725421f15f2365d08d303
Brogy-alt <brogangys2@gmail.com>
2023-03-03 14:24:48 +0200

    fixed calling out prodName

--- model/index.js
+++ model/index.js
@@ -142,7 +142,7 @@ deleteUser(req, res) {
 // Product
 class Product {
     fetchProducts(req, res) {
-        const strQry = `SELECT productID, prodGenre, prodDescription, prodArtist, price,
+        const strQry = `SELECT productID, prodName, prodGenre, prodDescription, prodArtist, price,
         prodQuantity, imgURL 
         FROM Products;`;
         db.query(strQry, (err, results)=> {
@@ -152,7 +152,7 @@ class Product {
     }
     fetchProduct(req, res) { 
         const strQry = `SELECT productId, prodName, prodGenre, prodDescription, prodArtist, price,
-        prodQuantity, imgURL
+        prodQuantity, imgURL, 
         FROM Products
         WHERE productID = ?;`;
         db.query(strQry, [req.params.id], (err, results)=> {
